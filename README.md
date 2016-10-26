dc2007e565ff1992a44a03495f649a94b3b44b3d
Tomer Sagi <tomer.sagi@hpe.com>
2016-10-26 15:05:59 +0300

    openjdk 8 compiling with this version

--- README.md
+++ README.md
@@ -3,10 +3,16 @@ This repository contains a patch script designed to allow java programmers to cr
 It is assumed [Quartz](https://github.com/HewlettPackard/quartz) is compiled and installed before running the script. 
 
 ## Requisites
-Some version of open-jdk-8 source code. Can be obtained from the project's [mercurial](http://hg.openjdk.java.net/jdk8u) repository. 
+Some version of open-jdk-8 source code. Can be obtained from the project's [mercurial](http://hg.openjdk.java.net/jdk8u) repository.
+Easiest way I found (once mercury is installed), is to run the following commands:
+	hg clone http://hg.openjdk.java.net/jdk8u/jdk8u <jdk_source_will_be_in_this_dir>
+	cd <jdk_source_will_be_in_this_dir>
+	bash ./get_source.sh 
 
 ## Usage
 1. Compile Quartz, install the library (libnvmemul.so) to the machine's \usr\lib\. Set-up the nvmemul device. 
-2. Run ```./openjdk8patcher.sh path/to/openjdk/src```
+2. copy the pmalloc to somewhere GCC will find it, i.e:
+	cp <path_to_quartz>/src/lib/pmalloc.h /usr/include/
+2. Run ```./openjdk8patcher.sh <jdk_source_will_be_in_this_dir>``` (after copying the script and DirectByteBufferFTM.java, unsafeFTM.cpp & UnsafeFTM.javato the same directory)
 3. build openjdk from source using its embedded readme. 
 4. Test your jdk by compiling and running the two simple tests in the unitTests directory. 
--- openjdk8patcher.sh
+++ openjdk8patcher.sh
@@ -16,16 +16,42 @@ echo 'This script modifies an openjdk installation to allow it to use the HPE Qu
 		echo "FATAL: couldn't find a valid openjdk source directory in supplied path: $SRCD"
 		exit
 	fi
-# Validation 2 - Quartz exists on this machine and in the proper path
+# TODO Validation 2 - Quartz exists on this machine and in the proper path
+	if [[ -f "/usr/lib/libnvmemul.so" ]]; then
+		echo "Quartz exists on this machine"
+	else
+		echo "FATAL: Quartz doesn't exist on this machine, couldn't find the file /usr/lib/libnvmemul.so"
+		exit
+	fi
 
 # 1. Modify hotspot files
 
-	# Modify hotspot/make/linux/makefiles/vm.make: replace 'LIBS += -lm -ldl -lpthread' with 'LIBS += -lm -ldl -lnvmemul -lpthread'
+	# TODO Modify hotspot/make/linux/makefiles/vm.make: replace 'LIBS += -lm -ldl -lpthread' with 'LIBS += -lm -ldl -lnvmemul -lpthread'
+	if [[ -f "$SRCD/hotspot/make/linux/makefiles/vm.make" ]]; then
+		sed -i '/LIBS += -lm -ldl -lpthread/c\LIBS += -lm -ldl -lnvmemul -lpthread' $SRCD/hotspot/make/linux/makefiles/vm.make
+		echo "updated the file hotspot/make/linux/makefiles/vm.make"
+	else 
+		echo "hotspot/make/linux/makefiles/vm.make is missing, and was not updated"
+	fi
 	
-	# Modify hotspot/src/share/vm/prims/nativeLookup.cpp :
+	# TODO Modify hotspot/src/share/vm/prims/nativeLookup.cpp :
+	if [[ -f "$SRCD/hotspot/src/share/vm/prims/nativeLookup.cpp" ]]; then
 		# 1. Add the row 'void JNICALL JVM_RegisterUnsafeMethods_p(JNIEnv *env, jclass unsafecls);' after the row 'void JNICALL JVM_RegisterUnsafeMethods(JNIEnv *env, jclass unsafecls);'
+		sed -i '/void JNICALL JVM_RegisterUnsafeMethods(JNIEnv \*env, jclass unsafecls);/avoid JNICALL JVM_RegisterUnsafeMethods_p(JNIEnv \*env, jclass unsafecls);' $SRCD/hotspot/src/share/vm/prims/nativeLookup.cpp
 		# 2. Add the text ' { CC"Java_sun_misc_UnsafeFTM_registerNatives", NULL, FN_PTR(JVM_RegisterUnsafeMethods_p) },' after the text '{ CC"Java_java_lang_invoke_MethodHandleNatives_registerNatives", NULL, FN_PTR(JVM_RegisterMethodHandleMethods) },'
-	# Add hotspot/src/share/vm/prims/unsafeFTM.cpp
+		sed -i '/{ CC"Java_java_lang_invoke_MethodHandleNatives_registerNatives", NULL, FN_PTR(JVM_RegisterMethodHandleMethods) },/a{ CC"Java_sun_misc_UnsafeFTM_registerNatives", NULL, FN_PTR(JVM_RegisterUnsafeMethods_p) },' $SRCD/hotspot/src/share/vm/prims/nativeLookup.cpp
+		echo "updated the file hotspot/src/share/vm/prims/nativeLookup.cpp"
+	else 
+		echo "hotspot/src/share/vm/prims/nativeLookup.cpp is missing, and was not updated"
+	fi
+		
+	# TODO Add hotspot/src/share/vm/prims/unsafeFTM.cpp
+	if [[ -f "./UnsafeFTM.cpp" ]]; then
+		cp ./UnsafeFTM.cpp $SRCD/hotspot/src/share/vm/prims/
+		echo "added hotspot/src/share/vm/prims/UnsafeFTM.cpp"
+	else
+		echo "UnsafeFTM.cpp is missing from current directory, and was not added"
+	fi
 
 	
 #2. Modify jdk files
@@ -44,7 +70,7 @@ echo 'This script modifies an openjdk installation to allow it to use the HPE Qu
 	else 
 		echo "jdk/make/java/nio/FILES_java.gmk is missing, and was not updated"
 	fi
-	# Modify jdk/src/share/classes/java/nio/Buffer.java
+
 	# Add jdk/src/share/classes/java/nio/DirectByteBufferFTM.java
 	if [[ -f "./DirectByteBufferFTM.java" ]]; then
 		cp ./DirectByteBufferFTM.java $SRCD/jdk/src/share/classes/java/nio/
@@ -59,4 +85,10 @@ echo 'This script modifies an openjdk installation to allow it to use the HPE Qu
 	else
 		echo "UnsafeFTM.java is missing from current directory, and was not added"
 	fi
-	# Modify jdk/make/tools/sharing/classlist.linux - add row 'sun/misc/UnsafeFTM' after row 'sun/misc/Unsafe'
+	# TODO Modify jdk/make/tools/sharing/classlist.linux - add row 'sun/misc/UnsafeFTM' after row 'sun/misc/Unsafe'
+	if [[ -f "$SRCD/jdk/make/tools/sharing/classlist.linux" ]]; then
+		sed -i '/sun\/misc\/Unsafe/a sun\/misc\/UnsafeFTM' $SRCD/jdk/make/tools/sharing/classlist.linux
+		echo "updated the file jdk/make/tools/sharing/classlist.linux"
+	else 
+		echo "jdk/make/tools/sharing/classlist.linux is missing, and was not updated"
+	fi
